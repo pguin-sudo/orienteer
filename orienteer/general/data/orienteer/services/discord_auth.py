853e6e9616083a0f89846e035602c6511861cb5d
PGuin <138515193+pguin-sudo@users.noreply.github.com>
2024-09-16 11:06:30 +0500

    Cached discord_auth

--- orienteer/general/data/orienteer/services/discord_auth.py
+++ orienteer/general/data/orienteer/services/discord_auth.py
@@ -1,15 +1,20 @@
 from uuid import UUID
 
+from aiocache import cached
+from aiocache.serializers import PickleSerializer
+
 from ..database import database_helper
 from ..models.discord_auth import DiscordAuth
 from ..repositories import discord_auth
 
 
+@cached(ttl=3600, serializer=PickleSerializer())
 async def is_discord_linked(user_id: UUID) -> bool:
     async with database_helper() as db_session:
         return await discord_auth.is_discord_linked(db_session, user_id)
 
 
+@cached(ttl=3600, serializer=PickleSerializer())
 async def link_discord(
     user_id: UUID, discord_user_id: int, discord_username: str
 ) -> None:
@@ -19,11 +24,13 @@ async def link_discord(
         )
 
 
+@cached(ttl=3600, serializer=PickleSerializer())
 async def get_discord_user_id_by_user_id(user_id: UUID) -> int | None:
     async with database_helper.session_factory() as db_session:
         return await discord_auth.get_discord_user_id_by_user_id(db_session, user_id)
 
 
+@cached(ttl=3600, serializer=PickleSerializer())
 async def get_user_id_by_discord_user_id(discord_user_id: int) -> UUID | None:
     async with database_helper.session_factory() as db_session:
         return await discord_auth.get_user_id_by_discord_user_id(
@@ -31,6 +38,7 @@ async def get_user_id_by_discord_user_id(discord_user_id: int) -> UUID | None:
         )
 
 
+@cached(ttl=3600, serializer=PickleSerializer())
 async def get_all_authorized() -> tuple[DiscordAuth, ...]:
     async with database_helper.session_factory() as db_session:
         return await discord_auth.get_all_authorized(db_session)
