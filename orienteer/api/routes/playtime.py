f7d48125b6ffcb84bdd44ec46387b2717c38499d
pguin-sudo <mailfordevelopmentandstuff@gmail.com>
2024-09-30 19:45:05 +0500

    Playtime token verify

--- orienteer/api/routes/playtime.py
+++ orienteer/api/routes/playtime.py
@@ -5,6 +5,7 @@ from fastapi.responses import Response
 
 from orienteer.general.data.orienteer.services import transactions
 from orienteer.general.utils.dtos import UserDTO
+from orienteer.general.config import PLAYTIME_TOKEN
 
 router = APIRouter()
 
@@ -12,6 +13,9 @@ router = APIRouter()
 @router.post("/api/playtime")
 async def sponsor_info_handler_route(request: Request):
     request = await request.json()
+
+    if request["token"] != PLAYTIME_TOKEN:
+        return Response(status_code=401)
 
     await transactions.add_orientiks_from_playtime(
         user_dto=await UserDTO.from_user_id(UUID(request["user_id"])),
