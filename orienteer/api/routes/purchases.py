7733c7fa3f3dd120974fd4c1bd8ff8c924ff2b1d
Requiem <ilya200012@gmail.com>
2025-02-26 16:52:16 +0300

    New endpoint for balance

--- orienteer/api/routes/purchases.py
+++ orienteer/api/routes/purchases.py
@@ -10,18 +10,18 @@ router = APIRouter()
 async def handle_purchase(request: Request):
     token = request.headers.get("Authorization")
     if not token or token != f"Bearer {TOKEN_PURCHASE}":
-        raise HTTPException(status_code=401, detail="Wrong token")
+        raise HTTPException(status_code=401, detail="[purchases]: Wrong token")
 
     data = await request.json()
     ckey = data.get("ckey")
     price = data.get("price")
 
     if not ckey or price is None:
-        raise HTTPException(status_code=400, detail="Incorrect data")
+        raise HTTPException(status_code=400, detail="[purchases]: Incorrect data")
 
     user_id = await get_user_id(ckey)
     if user_id is None:
-        raise HTTPException(status_code=404, detail="No player with this ckey")
+        raise HTTPException(status_code=404, detail="[purchases]: No player with this ckey")
 
     current_balance = await get_balance(user_id)
     if current_balance < price:
@@ -35,5 +35,27 @@ async def handle_purchase(request: Request):
 
     return JSONResponse(
         content={"Success": True, "Message": "Purchase complete successfully!", "NewBalance": float(new_balance)},
+        status_code=200
+    )
+
+@router.post("/api/purchases/balance")
+async def get_balance_request(request: Request):
+    token = request.headers.get("Authorization")
+    if not token or token != f"Bearer {TOKEN_PURCHASE}":
+        raise HTTPException(status_code=401, detail="[balance]: Wrong token")
+
+    data = await request.json()
+    ckey = data.get("ckey")
+
+    if not ckey:
+        raise HTTPException(status_code=400, detail="[balance]: Incorrect data")
+
+    user_id = await get_user_id(ckey)
+    if user_id is None:
+        raise HTTPException(status_code=404, detail="[balance]: No player with this ckey")
+
+    balance = await get_balance(user_id)
+    return JSONResponse(
+        content={"Success": True, "Balance": float(balance)},
         status_code=200
     )
