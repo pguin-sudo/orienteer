1366d9d6522579e9fc90b6c0dd0dc9bd8ac2fe5f
Requiem <ilya200012@gmail.com>
2025-02-28 15:06:20 +0300

    Correction of comments
    1) Renamed token
    2) Changed Post to Get for balance
    3) PascalCase changed to camelCase
    4) Convert to float deleted
    5) Another way to endpoint
    
    TODO before release: delete "detail"s from network errors

--- orienteer/api/routes/orientiks.py
+++ orienteer/api/routes/orientiks.py
@@ -1,32 +1,32 @@
 from fastapi import APIRouter, Request, HTTPException
 from fastapi.responses import JSONResponse
-from orienteer.general.config import TOKEN_ORIENTIKS
+from orienteer.general.config import ORIENTIKS_SECRET_KEY
 from orienteer.general.data.orienteer.services.transactions import get_balance, spend
 from orienteer.general.data.ss14.repositories.player import get_user_id
 
 router = APIRouter()
 
-@router.post("/api/orientiks")
+@router.post("/api/orientiks/purchases")
 async def handle_purchase(request: Request):
     token = request.headers.get("Authorization")
-    if not token or token != f"Bearer {TOKEN_ORIENTIKS}":
-        raise HTTPException(status_code=401, detail="[purchases]: Wrong token")
+    if not token or token != f"Bearer {ORIENTIKS_SECRET_KEY}":
+        raise HTTPException(status_code=401, detail="[orientiks]: Wrong token")
 
     data = await request.json()
     ckey = data.get("ckey")
     price = data.get("price")
 
     if not ckey or price is None:
-        raise HTTPException(status_code=400, detail="[purchases]: Incorrect data")
+        raise HTTPException(status_code=400, detail="[orientiks]: Incorrect data")
 
     user_id = await get_user_id(ckey)
     if user_id is None:
-        raise HTTPException(status_code=404, detail="[purchases]: No player with this ckey")
+        raise HTTPException(status_code=404, detail="[orientiks]: No player with this ckey")
 
     current_balance = await get_balance(user_id)
     if current_balance < price:
         return JSONResponse(
-            content={"Success": False, "Message": "Not enough money", "NewBalance": float(current_balance)},
+            content={"success": False, "message": "Not enough money", "newBalance": current_balance},
             status_code=400
         )
 
@@ -34,18 +34,15 @@ async def handle_purchase(request: Request):
     new_balance = await get_balance(user_id)
 
     return JSONResponse(
-        content={"Success": True, "Message": "Purchase complete successfully!", "NewBalance": float(new_balance)},
+        content={"success": True, "message": "Purchase complete successfully!", "newBalance": new_balance},
         status_code=200
     )
 
-@router.post("/api/orientiks/balance")
-async def get_balance_request(request: Request):
+@router.get("/api/orientiks/balance")
+async def get_balance_request(ckey: str, request: Request):
     token = request.headers.get("Authorization")
-    if not token or token != f"Bearer {TOKEN_ORIENTIKS}":
+    if not token or token != f"Bearer {ORIENTIKS_SECRET_KEY}":
         raise HTTPException(status_code=401, detail="[balance]: Wrong token")
-
-    data = await request.json()
-    ckey = data.get("ckey")
 
     if not ckey:
         raise HTTPException(status_code=400, detail="[balance]: Incorrect data")
@@ -56,6 +53,6 @@ async def get_balance_request(request: Request):
 
     balance = await get_balance(user_id)
     return JSONResponse(
-        content={"Success": True, "Balance": float(balance)},
+        content={"success": True, "balance": balance},
         status_code=200
     )
