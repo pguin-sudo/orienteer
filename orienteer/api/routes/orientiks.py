e14d5deb91b6a3310300a036d8ad6b56433fca2d
PGuin <138515193+pguin-sudo@users.noreply.github.com>
2025-03-05 12:30:19 +0000

    Fix routes/oriektiks.py

--- orienteer/api/routes/orientiks.py
+++ orienteer/api/routes/orientiks.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Request, HTTPException
 from fastapi.responses import JSONResponse
+
 from orienteer.general.config import ORIENTIKS_SECRET_KEY
-from orienteer.general.data.orienteer.services.transactions import get_balance, spend
-from orienteer.general.data.ss14.repositories.player import get_user_id
+from orienteer.general.data.orienteer.services import transactions, player
 
 router = APIRouter()
 
@@ -19,19 +19,19 @@ async def add_purchase(request: Request):
     if not ckey or price is None:
         raise HTTPException(status_code=400, detail="[orientiks]: Incorrect data")
 
-    user_id = await get_user_id(ckey)
+    user_id = await player.get_user_id(ckey)
     if user_id is None:
         raise HTTPException(status_code=404, detail="[orientiks]: No player with this ckey")
 
-    current_balance = await get_balance(user_id)
+    current_balance = await transactions.get_balance(user_id)
     if current_balance < price:
         return JSONResponse(
             content={"message": "Not enough money", "newBalance": current_balance},
             status_code=400
         )
 
-    await spend(user_id, price)
-    new_balance = await get_balance(user_id)
+    await transactions.spend(user_id, price)
+    new_balance = await transactions.get_balance(user_id)
 
     return JSONResponse(
         content={"message": "Purchase completed successfully!", "newBalance": new_balance},
@@ -47,11 +47,11 @@ async def get_balance(ckey: str, request: Request):
     if not ckey:
         raise HTTPException(status_code=400, detail="[balance]: Incorrect data")
 
-    user_id = await get_user_id(ckey)
+    user_id = await player.get_user_id(ckey)
     if user_id is None:
         raise HTTPException(status_code=404, detail="[balance]: No player with this ckey")
 
-    balance = await get_balance(user_id)
+    balance = await transactions.get_balance(user_id)
     return JSONResponse(
         content={"balance": balance},
         status_code=200
